02939bb695711793c743a19fa38f3f8d5a78b639
Jackson da Cruz <jackson.cruz@temporario.com.br>
2016-05-27 12:39:21 -0300

    Fibonacci Modified

---
+++ HackerRank/src/com/jackson/hackerrank/FibonacciModified.java
@@ -0,0 +1,28 @@
+package com.jackson.hackerrank;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FibonacciModified {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int n = in.nextInt();
+        fibonacci(a,b,n);
+    }
+    
+    public static void fibonacci(int a, int b, int n) {
+        int start = n-3;
+        BigInteger oldDigit = BigInteger.valueOf(a);
+        BigInteger lastDigit = BigInteger.valueOf(b);
+        for (int i = 0; i <= start; i++) {
+            BigInteger temp = lastDigit;
+            lastDigit = oldDigit.add(lastDigit.pow(2));
+            oldDigit = temp;
+        }
+        System.out.println(lastDigit);
+    }
+
+}
